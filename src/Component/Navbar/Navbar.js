4867f54cb67b744823f78dd5548327b5cdd7dcc2
Romain Caillat <caillat.romain@epitech.eu>
2022-12-08 20:38:46 +0100

    fix all button animation and add underline on button and 🩹 fix the react router link

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,34 +1,62 @@
 import React from 'react'
-import { Navbar, Button } from '@nextui-org/react'
+import { Navbar, Button, Text } from '@nextui-org/react'
 import { useProSidebar } from 'react-pro-sidebar'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 export default function NavbarComponent() {
-  // const collapseItems = [
-  //   "Home",
-  //   "Profile",
-  //   "Login",
-  //   "Register"
-  // ]
+  let location = useLocation()
+
+
+  function checkLocation(path) {
+    if (location.pathname === path)
+      return true
+    else
+      return false
+  }
 
   const { collapseSidebar } = useProSidebar()
 
   return (
     <Navbar isBordered variant="sticky" maxWidth={"fluid"} blur='10px'>
         <Navbar.Brand>
-          <Button onClick={() => collapseSidebar()}>AGG</Button>
+          <Button auto onPress={() => collapseSidebar()}>AGG</Button>
         </Navbar.Brand>
-        <Navbar.Content hideIn="xs" variant={"underline"} enableCursorHighlight>
-            <Navbar.Link  href="/">Home</Navbar.Link>
-            <Navbar.Link href="/profil/test">profil</Navbar.Link>
+        <Navbar.Content hideIn="xs" variant={"underline"} >
+            <Navbar.Item isActive={checkLocation("/")}>
+              <Button auto light animated='false'>
+                <Link to="/">
+                  <Text color='white'>
+                    Home
+                  </Text>
+                </Link>
+              </Button>
+            </Navbar.Item>
+            <Navbar.Item isActive={checkLocation("/profil/test")}>
+              <Button auto light animated='false'>
+                <Link to="/profil/test">
+                  <Text color='white'>
+                    Profil
+                  </Text>
+                </Link>
+              </Button>
+            </Navbar.Item>
         </Navbar.Content>
-        <Navbar.Content variant={"underline"} enableCursorHighlight>
-        
-            {/* <Navbar.Link href="/login">Login</Navbar.Link> */}
-            <Button auto flat>
-              <Link to="./register">Sign Up</Link>
+        <Navbar.Content>
+          <Navbar.Item isActive={checkLocation("/login")} variant={"underline"}>
+            <Button auto light>
+              <Link to="/login">
+                <Text color='white'>
+                  Login
+                </Text>
+              </Link>
             </Button>
-            {/* <Navbar.Link href="/register">Register</Navbar.Link> */}
+          </Navbar.Item>
+          <Navbar.Item isActive={checkLocation("/register")}>
+            
+            <Button auto flat>
+              <Link to="./register"> <Text color='white'>Sign Up</Text></Link>
+            </Button>
+          </Navbar.Item>
         </Navbar.Content>
     </Navbar>
   )
