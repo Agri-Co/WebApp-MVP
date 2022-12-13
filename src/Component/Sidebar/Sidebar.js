1ae332fa21ecc1816d169f8267499361efea31dd
nicolas humbert <nicolas.humbert@epitech.eu>
2022-12-13 13:06:47 +0100

    💄 add new button to open the dashboard page

--- src/Component/Sidebar/Sidebar.js
+++ src/Component/Sidebar/Sidebar.js
@@ -2,9 +2,20 @@ import React from 'react'
 import { Menu, MenuItem, Sidebar } from 'react-pro-sidebar'
 import { Button } from '@nextui-org/react'
 import { useWindowSize } from 'react-use'
+import { useLocation, useNavigate } from 'react-router-dom'
+import {goto} from '../Navbar/Navbar'
+
 
 function SidebarComponent() {
-    const {height} = useWindowSize()
+
+    const {height} = useWindowSize();
+    const navigate = useNavigate();
+
+    function handleClick()
+    {
+        navigate("/dashboard");
+    }
+
 
     return (
         <div className='SidebarComponent'
@@ -18,8 +29,19 @@ function SidebarComponent() {
             {console.log(height)}
             <Sidebar transitionDuration={1000}>
                 <Menu>
-                    <MenuItem> <Button>TEST</Button> </MenuItem>
-                    <MenuItem>TEST 2</MenuItem>
+                    <MenuItem> 
+                        <Button>
+                            TEST
+                        </Button> 
+                    </MenuItem>
+                    <MenuItem>
+                        <Button onClick={() => handleClick()}>
+                            DASHBOARD
+                        </Button>
+                    </MenuItem>
+                    <MenuItem>
+                        TEST 2
+                    </MenuItem>
                 </Menu>
             </Sidebar>
         </div>
