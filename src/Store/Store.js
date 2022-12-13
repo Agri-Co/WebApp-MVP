8b95d3e5f0b717b7e2b971cce6608f735ad06340
Romain Caillat <caillat.romain@epitech.eu>
2022-12-13 11:50:01 +0100

    🔥 CounterSlice import

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from '../Features/counterSlice'
 import profilReducer from '../Features/profilSlice'
 
 export default configureStore({
