5f183f99fc00f4b40e61c1efe4df939802355c67
Romain Caillat <caillat.romain@epitech.eu>
2022-12-15 14:55:56 +0100

    🚀 deploy middleware to set and to prevented to message to server

--- src/Websocket/Client.js
+++ src/Websocket/Client.js
@@ -1,20 +1,52 @@
 import { useWebSocket } from "react-use-websocket/dist/lib/use-websocket"
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setwatering } from "../Features/WateringSlice";
 import { sethumidity } from "../Features/HumiditySlice"
+import { useEffect } from "react";
 
-const socketUrl = 'ws://localhost:3001'; 
+const socketUrl = 'ws://localhost:3001';
+
+var actioncount = 0
+
+export const loggerMiddleware = (store) => (next) => (action) => {
+    // console.log("action", action);
+    if (action.type === 'profil/setemplacement') {
+        actioncount = 1
+        // console.log(action)
+
+    }
+        return next(action)
+}
 
 export function WebSocketClient() {
+    const emplacement = useSelector(state => state.profil.emplacement)
+    const humidity = useSelector(state => state.humiditydata.humidity)
+    // console.log("action: ",actioncount)
     const dispatch = useDispatch()
-    const {lastJsonMessage} = useWebSocket(socketUrl, {onOpen: () => console.log('open'), shouldReconnect: (CloseEvent) => true,})
-    // sendMessage('hello')
-    if (lastJsonMessage !== null)
-        for (var i in lastJsonMessage) {
-            console.log("jsonmessage content:", i)
-            if (i === "humidity")
-                dispatch(sethumidity(lastJsonMessage[i]))
-            if (i === "nextWatering")
-                dispatch(setwatering(lastJsonMessage[i]))
+    const {sendMessage , lastJsonMessage} = useWebSocket(socketUrl, {onOpen: () => console.log('open'), shouldReconnect: (CloseEvent) => true,})
+
+    useEffect(() => {
+        switch (actioncount) {
+            case 0:
+                if (lastJsonMessage !== null)
+                // console.log(lastJsonMessage)
+                for (var i in lastJsonMessage) {
+                    // console.log("jsonmessage content:", i)
+                    if (i === "humidity") {
+                        dispatch(sethumidity(lastJsonMessage[i]))
+                        console.log("humidity:", humidity)
+                    }
+                    if (i === "nextWatering")
+                        dispatch(setwatering(lastJsonMessage[i]))
+                }
+                break
+            case 1:
+                if (actioncount === 1) {
+                    // console.log(emplacement)
+                    actioncount = 0
+                    // console.log("actino afet action", actioncount)
+                }
+                break
         }
+    })    
 }
