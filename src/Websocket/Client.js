b052ae8c229b107a0582b20615247f8268158572
Clement Mikula <clement.mikula@epitech.eu>
2022-12-15 16:08:15 +0100

    👽️ change api to send location to the backend

--- src/Websocket/Client.js
+++ src/Websocket/Client.js
@@ -15,38 +15,47 @@ export const loggerMiddleware = (store) => (next) => (action) => {
         // console.log(action)
 
     }
-        return next(action)
+    return next(action)
 }
 
 export function WebSocketClient() {
     const emplacement = useSelector(state => state.profil.emplacement)
     const humidity = useSelector(state => state.humiditydata.humidity)
+    const json_emplacement = {
+        emplacement
+    }
+
     // console.log("action: ",actioncount)
     const dispatch = useDispatch()
-    const {sendMessage , lastJsonMessage} = useWebSocket(socketUrl, {onOpen: () => console.log('open'), shouldReconnect: (CloseEvent) => true,})
+    const { sendMessage, sendJsonMessage, lastJsonMessage } = useWebSocket(socketUrl, { onOpen: () => console.log('open'), shouldReconnect: (CloseEvent) => true, })
+    console.log("DeBUG1")
 
     useEffect(() => {
         switch (actioncount) {
             case 0:
+                // sendJsonMessage(json_emplacement) // remove
                 if (lastJsonMessage !== null)
-                // console.log(lastJsonMessage)
-                for (var i in lastJsonMessage) {
-                    // console.log("jsonmessage content:", i)
-                    if (i === "humidity") {
-                        dispatch(sethumidity(lastJsonMessage[i]))
-                        console.log("humidity:", humidity)
+                    // console.log(lastJsonMessage)
+                    for (var i in lastJsonMessage) {
+                        // console.log("jsonmessage content:", i)
+                        if (i === "humidity") {
+                            dispatch(sethumidity(lastJsonMessage[i]))
+                            console.log("humidity:", humidity)
+                        }
+                        if (i === "nextWatering")
+                            dispatch(setwatering(lastJsonMessage[i]))
                     }
-                    if (i === "nextWatering")
-                        dispatch(setwatering(lastJsonMessage[i]))
-                }
                 break
             case 1:
+                console.log("DeBUG2")
                 if (actioncount === 1) {
                     // console.log(emplacement)
+                    // sendMessage(emplacement)
+                    sendJsonMessage(json_emplacement)
                     actioncount = 0
                     // console.log("actino afet action", actioncount)
                 }
                 break
         }
-    })    
+    })
 }
